f9bf2042ca1ed4e74ec0469893cbac41af027682
Пётр Иванов <petara94@mail.ru>
2021-12-08 01:46:00 +0300

    add buttons & funcs to open frmProc

--- Laba4/Form1.cs
+++ Laba4/Form1.cs
@@ -16,5 +16,30 @@ namespace Laba4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new frmProc().Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
